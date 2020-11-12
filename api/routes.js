da87b8e15b0775236a94db31b0e8b823e476bf96
Abel Roland <abelroland@gmail.com>
2020-11-12 19:07:19 +0100

    Creating routes This closes #11

--- api/routes.js
+++ api/routes.js
@@ -6,6 +6,9 @@ const router = express.Router();
 router.get('/', controllers.hello);
 
 // write your routes
-
+router.get('/api', controllers.allEntries);
+router.get('/api:id', controllers.singleEntry);
+router.post('/api', controllers.newPet);
+router.delete('/api:id', controllers.deleteEntry);
 
 module.exports = router;
